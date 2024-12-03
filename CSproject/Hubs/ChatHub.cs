8605692db29a24b56ab9b00ede22a6e16469bc34
Степан <elkinstepan405@gmail.com>
2024-12-03 20:06:30 +0500

    Настроена отправка сообщений

--- CSproject/Hubs/ChatHub.cs
+++ CSproject/Hubs/ChatHub.cs
@@ -5,40 +5,89 @@ namespace CSproject.Hubs
 {
     public interface IChatClient
     {
-        public Task RecieveMessage(string username, string message);
+        public Task RecieveMessage(string username, string message , bool isMyMessage);
     }
 
-    public record UserConnection(string username, string chatRoom);
+    public record UserConnection(string Username, string ChatRoom , string Uuid);
+    public record Connection( string ConnectionId, string Uuid);
+
 
     public class ChatHub:Hub<IChatClient>
     {
-        private static MemoryCache _connections = new(new MemoryCacheOptions());
+        private static readonly Dictionary<string , UserConnection> _UserConnections = new();
+        private static readonly Dictionary<string , string> _Connections = new();
 
 
-        public async Task JoinChat(string username , string chatRoom)
+        public void Connect(string uuid)
         {
-            _connections.Set(Context.ConnectionId, new UserConnection(username, chatRoom));
-            //Console.WriteLine(Context.ConnectionId);
+            
+            if (!_Connections.ContainsValue(Context.ConnectionId))
+            {
+                if (!_Connections.ContainsKey(uuid))
+                {
+                    _Connections.Add(Context.ConnectionId , uuid);
+                }
+                else
+                {
+                    SetConnection(uuid );
+                }
+            }
+        }
+
+        private async void SetConnection(string uuid)
+        {
+            var chatRoom = _UserConnections[uuid].ChatRoom;
+            var oldConnection = _Connections[uuid];
+            await Groups.RemoveFromGroupAsync(oldConnection, chatRoom);
+            _Connections.Remove(uuid);
+
+            _Connections.Add(uuid , Context.ConnectionId);
             await Groups.AddToGroupAsync(Context.ConnectionId, chatRoom);
+        }
+
+        private async void Disconnect(string uuid)
+        {
+            var chatRoom = _UserConnections[uuid].ChatRoom;
+            _UserConnections.Remove(uuid);
+            await Groups.RemoveFromGroupAsync(_Connections[uuid], chatRoom);
+            _Connections.Remove(uuid);
+        }
+
+        public void LoginUser(string username, string chatRoom, string uuid)
+        {
+            _UserConnections.Add(uuid, new UserConnection(username, chatRoom, uuid));
+        }
+
+        public async Task JoinChat( string uuid)
+        {
+            var user = _UserConnections[uuid];
+            await Groups.AddToGroupAsync(Context.ConnectionId, user.ChatRoom);
+            
             await Clients
-                .Group(chatRoom)
-                .RecieveMessage("Admin", $"{username} присоединился к чату!");
+                .Group(user.ChatRoom)
+                .RecieveMessage("Admin", $"{user.Username} присоединился к чату!" , false);
         }
 
 
-        public async Task SendMessage(string message)
+        public async Task SendMessage(string message , string uuid)
         {
-            UserConnection connection = (UserConnection)_connections.Get(Context.ConnectionId);
-            //Console.WriteLine(Context.ConnectionId.ToString());
-            Console.WriteLine(connection);
-            //Console.WriteLine(_connections.Count);
+            UserConnection connection;
+
+            if (_UserConnections.ContainsKey(uuid))
+                connection = _UserConnections[uuid];
+            else throw new ArgumentException($"{uuid} not in UserConnection dict! ");
+
+
+            //Console.WriteLine(connection);
+
 
             if (connection is not null)
             {
                 await Clients
-                    .Group(connection.chatRoom)
-                    .RecieveMessage(connection.username,message);
+                    .Group(connection.ChatRoom)
+                    .RecieveMessage(connection.Username, message , true);
             }
+            else throw new ArgumentException($"{Context.ConnectionId} not in active Connections dict!");
         }
     }
 }
