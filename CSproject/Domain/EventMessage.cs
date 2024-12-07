150c5d4222b6d86bd46588dd840d03124122e25a
Nik1072 <nikb107701@gmail.com>
2024-12-07 15:28:01 +0500

    ---

---
+++ CSproject/Domain/EventMessage.cs
@@ -0,0 +1,21 @@
+﻿namespace CSproject.Domain
+{
+    public class EventMessage
+    {
+        public string Sender { get; set; }
+        public bool Status { get; set; }
+        public DateTime SendDate { get; set; }
+        public string Photo { get; set; }
+        public Dictionary<bool, string> Reactions { get; set; }
+        public string EventName { get; set; }
+        public string EventDescription { get; set; }
+        public DateTime EventDate { get; set; }
+        public string Location { get; set; }
+
+        public EventMessage(string senderName /*, ... */)
+        {
+            Sender = senderName;
+            SendDate = DateTime.Now;
+        }
+    }
+}
